142f07c9b6a9836b4eefb10eea7a68eede3c1ba5
Adan Escamilla <adanescamilla580@gmail.com>
2021-02-25 16:02:39 -0800

    commit tree 136e10e7e8c6d3b7e92cc48bd8fec7f4fee1fe95

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 let addToy = false;
 
+const urlToys = `http://localhost:3000/toys`  
+
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+  // const addBtn = document.querySelector("#new-toy-btn");
+  // const toyFormContainer = document.querySelector(".container");
+  // addBtn.addEventListener("click", () => {
+  //   // hide & seek with the form
+  //   addToy = !addToy;
+  //   if (addToy) {
+  //     toyFormContainer.style.display = "block";
+  //   } else {
+  //     toyFormContainer.style.display = "none";
+  //   }
+  // });
+  function loadAllToys(){
+    fetch(urlToys)
+    .then(response => console.log(response.json()))
+    .then(toys => console.log(toys))
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    
+  }
+  loadAllToys()
 });
+
+
+
